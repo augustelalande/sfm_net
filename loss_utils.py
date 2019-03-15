bac9730b455d7f70c6815530ba85790b83c4d064
augustelalande <auguste.lalande@gmail.com>
2019-03-15 17:25:15 -0400

    add forward backward consistency loss

--- loss_utils.py
+++ loss_utils.py
@@ -3,15 +3,7 @@ from tensorflow.losses import mean_squared_error as mse
 
 
 def frame_loss(x0, x1, points):
-    b, h, w, c = x0.shape
-
-    warp_x = points[:, :, :, 0]
-    warp_y = points[:, :, :, 1]
-
-    warp_x = warp_x * tf.cast(w - 1, tf.float32)
-    warp_y = warp_y * tf.cast(h - 1, tf.float32)
-    warp = tf.stack([warp_x, warp_y], -1)
-
+    warp = points_to_warp(points)
     x1_t = tf.contrib.resampler.resampler(x1, warp)
     return mse(x0, x1_t), x1_t
 
@@ -21,5 +13,18 @@ def spatial_smoothness_loss(x):
     return tf.reduce_mean(tf.square(gradients))
 
 
-def forward_backward_consistency_loss(x):
-    pass
+def forward_backward_consistency_loss(d0, d1, points, pc_t):
+    warp = points_to_warp(points)
+    d1_t = tf.contrib.resampler.resampler(d1, warp)
+    Z0 = pc_t[:, :, :, 2:3]
+    return mse(d1_t, Z0)
+
+
+def points_to_warp(points):
+    b, h, w, c = points.shape
+    warp_x = points[:, :, :, 0]
+    warp_y = points[:, :, :, 1]
+
+    warp_x = warp_x * tf.cast(w - 1, tf.float32)
+    warp_y = warp_y * tf.cast(h - 1, tf.float32)
+    return tf.stack([warp_x, warp_y], -1)
